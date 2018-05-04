7539ea207344c6e3252b723de3aa4aac0a0e235a
apoc64 <sschwedt@gmail.com>
2018-05-04 10:42:03 -0600

    shows a list of comedains specials

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,4 @@
 class Comedian < ActiveRecord::Base
+  has_many :specials
 
 end
--- spec/features/user_sees_comedians_spec.rb
+++ spec/features/user_sees_comedians_spec.rb
@@ -3,15 +3,20 @@ RSpec.describe 'Visitor goes to comedian page' do
     iliza = Comedian.create(name: "Iliza Shlesinger", age: 34)
     tom = Comedian.create(name: "Tom Segura", age: 38)
     visit '/comedians'
-    # As a visitor,
-    # When I visit `/comedians`
-    # Then I see a list of comedians with the following
-    # information for each comedian:
-    # * Name
-    # * Age
     expect(page).to have_content('Iliza Shlesinger')
     expect(page).to have_content('34')
     expect(page).to have_content('Tom Segura')
     expect(page).to have_content('38')
+  end
+
+  it 'shows a list of comedian specials' do
+    iliza = Comedian.create(name: "Iliza Shlesinger", age: 34)
+    Special.create(name: "War Paint", comedian_id: iliza.id)
+    Special.create(name: "Freezing Hot", comedian_id: iliza.id)
+    Special.create(name: "Confirmed Kills", comedian_id: iliza.id)
+    visit '/comedians'
+    expect(page).to have_content('War Paint')
+    expect(page).to have_content('Freezing Hot')
+    expect(page).to have_content('Confirmed Kills')
   end
 end
