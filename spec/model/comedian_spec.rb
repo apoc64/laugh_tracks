51157d95ab2d05c6d06d4a500ebae17a3d10f75f
apoc64 <sschwedt@gmail.com>
2018-05-04 10:33:11 -0600

    tests creation of comedian

--- spec/model/comedian_spec.rb
+++ spec/model/comedian_spec.rb
@@ -1,8 +1,9 @@
-# RSpec.describe Comedian do
-#   describe 'create comedian' do
-#     it 'should create a valid comedian' do
-#       merchant = Merchant.create
-#       expect(merchant).to_not be_valid
-#     end
-#   end
-# end
+RSpec.describe Comedian do
+  describe 'create comedian' do
+    it 'should create a valid comedian' do
+      comedian = Comedian.create(name: "Iliza Shlesinger", age: 34)
+      expect(comedian).to be_valid
+      expect(Comedian.count).to eq(1)
+    end
+  end
+end
